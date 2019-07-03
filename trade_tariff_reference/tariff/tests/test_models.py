5d834db7a9ad9d25b49c436c3014c910dbfa034a
Marcus Patino Pan <marcus.patino-pan@digital.trade.gov.uk>
2019-07-03 15:17:02 +0100

    Add measure test and factory

--- trade_tariff_reference/tariff/tests/test_models.py
+++ trade_tariff_reference/tariff/tests/test_models.py
@@ -1,4 +1,10 @@
+import pytest
+
+from trade_tariff_reference.tariff.tests.factories import MeasureFactory
+
+pytestmark = pytest.mark.django_db
 
 
 def test_measures_model():
-    assert 1 == 2
+    measure = MeasureFactory(status='LOADED')
+    assert measure.status == 'LOADED'
