c598c67eca05a8fe1356029560708ef93e23d029
Marcus Patino Pan <marcus.patino-pan@digital.trade.gov.uk>
2019-07-11 09:15:50 +0100

    Basic Manage Schedule View

--- trade_tariff_reference/schedule/views.py
+++ trade_tariff_reference/schedule/views.py
@@ -1,1 +1,19 @@
-# Create your views here.
+from django.views.generic import TemplateView
+
+
+class ManageAgreementScheduleView(TemplateView):
+    template_name = 'schedule/manage.html'
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['agreements'] = [
+            {
+                'id': 'Israel',
+                'area_name': 'Israel',
+                'country_code': '1L',
+                'title': 'Agreement between the United Kingdom of Great Britain and Northern Ireland and Israel',
+                'date': '21 February 2019',
+                'version': '1.0'
+            }
+        ]
+        return context_data
