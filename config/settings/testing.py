ae7a4273140e0596e10fac712032c450f53ae9d8
Marcus Patino Pan <marcus.patino-pan@digital.trade.gov.uk>
2019-07-31 15:08:25 +0100

    Fixes for whitenoise making tests slow

--- config/settings/testing.py
+++ config/settings/testing.py
@@ -3,3 +3,13 @@ from .development import *
 MANAGE_TARIFF_DATABASE = True
 
 DATABASES['tariff']['HOST'] = env('POSTGRES_HOST')
+
+WHITENOISE_AUTOREFRESH = True
+
+DEBUG = False
+
+STATIC_ROOT = '/app/test_assets'
+
+GENERATED_DOCUMENT_LOCATION = '/app/test_assets/tariff/documents'
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
