1a8998f82c9fd5007bb79e6e3580f9867e3d92d7
Marcus Patino Pan <marcus.patino-pan@digital.trade.gov.uk>
2019-07-31 16:44:35 +0100

    Add SSO

--- config/urls.py
+++ config/urls.py
@@ -5,5 +5,6 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('trade_tariff_reference.core.urls', namespace='core')),
     path('schedule/', include('trade_tariff_reference.schedule.urls', namespace='schedule')),
+    path('auth/', include('authbroker_client.urls', namespace='authbroker')),
     path('admin/', admin.site.urls)
 ]
