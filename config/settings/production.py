0ed1df00cc996df4e5cb110cb5e91652d946b236
Marcus Patino Pan <marcus.patino-pan@digital.trade.gov.uk>
2019-08-05 14:03:20 +0100

    uri not url in db settings config

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,10 +5,10 @@ import dj_database_url
 
 VCAP_SERVICES = env('VCAP_SERVICES')
 
-DEFAULT_DATABASE_URL = VCAP_SERVICES['postgres'][0]['credentials']['url']
-TARIFF_DATABASE_URL = VCAP_SERVICES['postgres'][1]['credentials']['url']
+DEFAULT_DATABASE_URL = VCAP_SERVICES['postgres'][0]['credentials']['uri']
+TARIFF_DATABASE_URL = VCAP_SERVICES['postgres'][1]['credentials']['uri']
 
-REDIS_URL = VCAP_SERVICES['redis'][0]['credentials']['url']
+REDIS_URL = VCAP_SERVICES['redis'][0]['credentials']['uri']
 
 
 DATABASES = {
