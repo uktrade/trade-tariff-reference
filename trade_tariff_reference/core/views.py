96a12d7b001f33aa8b5f2afe173f6a5168fe46ed
Marcus Patino Pan <marcus.patino-pan@digital.trade.gov.uk>
2019-08-05 14:00:09 +0100

    Basic healthcheck added

--- trade_tariff_reference/core/views.py
+++ trade_tariff_reference/core/views.py
@@ -1,5 +1,19 @@
+import time
+
 from django.views.generic import TemplateView
 
 
 class HomepageView(TemplateView):
     template_name = 'core/index.html'
+
+
+class HealthCheckView(TemplateView):
+    template_name = 'core/healthcheck.html'
+
+    def get_context_data(self, **kwargs):
+        """ Adds status and response time to response context"""
+        context = super().get_context_data(**kwargs)
+        context['status'] = 'OK'
+        # nearest approximation of a response time
+        context['response_time'] = time.time()
+        return context
