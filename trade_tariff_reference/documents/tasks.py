c349872770c9ab0aef63d4b742286e7ba08301b9
Marcus Patino Pan <marcus.patino-pan@digital.trade.gov.uk>
2019-08-12 09:40:17 +0100

    Improve error handling generating documents

--- trade_tariff_reference/documents/tasks.py
+++ trade_tariff_reference/documents/tasks.py
@@ -1,12 +1,36 @@
 from celery import shared_task
 
 from trade_tariff_reference.documents.fta.application import Application
+from trade_tariff_reference.documents.utils import update_agreement_document_status
 from trade_tariff_reference.schedule.models import Agreement
 
 
-@shared_task
+def handle_document_generation_fail(self, exc, task_id, args, kwargs, einfo):
+    if not args:
+        return
+    slug = args[0]
+    try:
+        agreement = Agreement.objects.get(
+            slug=slug,
+            document_status=Agreement.GENERATING
+        )
+    except Agreement.DoesNotExist:
+        pass
+    else:
+        update_agreement_document_status(agreement, Agreement.UNAVAILABLE)
+
+
+@shared_task(
+    autoretry_for=(Exception,),
+    max_retries=3,
+    retry_backoff=30,
+    on_failure=handle_document_generation_fail,
+)
 def generate_document(country_profile, force=False):
-    app = Application(country_profile=country_profile, force_document_generation=force)
+    app = Application(
+        country_profile=country_profile,
+        force_document_generation=force,
+    )
     app.create_document()
     app.shutDown()
 
