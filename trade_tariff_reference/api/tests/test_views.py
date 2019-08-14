f908e8154368201aec709ecf5bc0a753bc7dfe21
Marcus Patino Pan <marcus.patino-pan@digital.trade.gov.uk>
2019-08-14 10:38:25 +0100

    Fixed API tests

--- trade_tariff_reference/api/tests/test_views.py
+++ trade_tariff_reference/api/tests/test_views.py
@@ -1,30 +1,11 @@
-from unittest import mock
-
+from django.shortcuts import reverse
 
 import pytest
-
-from rest_framework.test import APIClient
-from django.shortcuts import reverse
 
 from trade_tariff_reference.schedule.tests.factories import AgreementFactory
 
 
 pytestmark = pytest.mark.django_db
-
-
-@mock.patch('authbroker_client.utils.get_profile')
-@mock.patch('authbroker_client.utils.get_client')
-def authenticated_client_with_login(mock_get_profile, mock_get_client,  user):
-    mock_get_client.return_value = mock.MagicMock(authorized=True)
-    mock_get_profile.return_value = {'email': user.email, 'first_name': user.first_name, 'last_name': user.last_name}
-    client = APIClient()
-    client.login()
-    return client
-
-
-def authenticated_client(user):
-    client = APIClient()
-    return client
 
 
 def sort_list_result(items_list, key='slug'):
@@ -36,27 +17,24 @@ def sort_list_result(items_list, key='slug'):
 
 class TestAgreementAPIViews:
 
-    def test_empty_agreement_list_view(self, user):
-        client = authenticated_client(user)
-        response = client.get(reverse('api:agreement-list'), user=user)
+    def test_empty_agreement_list_view(self, authenticated_client):
+        response = authenticated_client.get(reverse('api:agreement-list'))
         assert response.status_code == 200
         assert response.json() == []
 
-    def test_agreement_list_view(self, user):
-        client = authenticated_client(user)
+    def test_agreement_list_view(self, authenticated_client):
         agreement_1 = AgreementFactory()
         agreement_2 = AgreementFactory()
-        response = client.get(reverse('api:agreement-list'), user=user)
+        response = authenticated_client.get(reverse('api:agreement-list'))
         assert response.status_code == 200
         actual_response = sort_list_result(response.json())
         assert len(actual_response) == 2
         self.assert_agreement(agreement_1, actual_response[agreement_1.slug])
         self.assert_agreement(agreement_2, actual_response[agreement_2.slug])
 
-    def test_agreement_detail_view(self, user):
-        client = authenticated_client(user)
+    def test_agreement_detail_view(self, authenticated_client):
         agreement = AgreementFactory()
-        response = client.get(reverse('api:agreement-detail', kwargs={'slug': agreement.slug}), user=user)
+        response = authenticated_client.get(reverse('api:agreement-detail', kwargs={'slug': agreement.slug}))
         assert response.status_code == 200
         actual_result = response.json()
         self.assert_agreement(agreement, actual_result)
