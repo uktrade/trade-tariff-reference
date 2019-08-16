b86de286e0a0e8650650ffd089d4fead97923d5c
Marcus Patino Pan <marcus.patino-pan@digital.trade.gov.uk>
2019-08-16 13:52:37 +0100

    Fix indents

--- trade_tariff_reference/documents/mfn/seasonal.py
+++ trade_tariff_reference/documents/mfn/seasonal.py
@@ -3,20 +3,19 @@ from trade_tariff_reference.documents import functions as f
 
 class Seasonal:
 
-	def __init__(
-		self, commodity_code="", season1_start="", season1_end="", season1_expression="", season2_start="",
-		season2_end="", season2_expression="", season3_start="", season3_end="", season3_expression=""
-	):
-		self.commodity_code = f.mstr(commodity_code)
-		self.season1_start = f.mstr(season1_start)
-		self.season1_end = f.mstr(season1_end)
-		self.season1_expression = f.format_seasonal_expression(season1_expression)
+    def __init__(
+        self, commodity_code="", season1_start="", season1_end="", season1_expression="", season2_start="",
+        season2_end="", season2_expression="", season3_start="", season3_end="", season3_expression=""
+    ):
+        self.commodity_code = f.mstr(commodity_code)
+        self.season1_start = f.mstr(season1_start)
+        self.season1_end = f.mstr(season1_end)
+        self.season1_expression = f.format_seasonal_expression(season1_expression)
 
-		self.season2_start = f.mstr(season2_start)
-		self.season2_end = f.mstr(season2_end)
-		self.season2_expression = f.format_seasonal_expression(season2_expression)
+        self.season2_start = f.mstr(season2_start)
+        self.season2_end = f.mstr(season2_end)
+        self.season2_expression = f.format_seasonal_expression(season2_expression)
 
-		self.season3_start = f.mstr(season3_start)
-		self.season3_end = f.mstr(season3_end)
-		self.season3_expression = f.format_seasonal_expression(season3_expression)
-
+        self.season3_start = f.mstr(season3_start)
+        self.season3_end = f.mstr(season3_end)
+        self.season3_expression = f.format_seasonal_expression(season3_expression)
--- trade_tariff_reference/documents/mfn/special.py
+++ trade_tariff_reference/documents/mfn/special.py
@@ -3,6 +3,6 @@ from trade_tariff_reference.documents import functions as f
 
 class Special:
 
-	def __init__(self, commodity_code="", note=""):
-		self.commodity_code = f.mstr(commodity_code)
-		self.note = f.mstr(note)
+    def __init__(self, commodity_code="", note=""):
+        self.commodity_code = f.mstr(commodity_code)
+        self.note = f.mstr(note)
