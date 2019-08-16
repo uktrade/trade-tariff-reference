b70327be62d24491b60b9daeabf3b55ee294c7d2
Marcus Patino Pan <marcus.patino-pan@digital.trade.gov.uk>
2019-08-16 12:47:50 +0100

    Fix for task import

--- trade_tariff_reference/schedule/utils.py
+++ trade_tariff_reference/schedule/utils.py
@@ -1,4 +1,4 @@
-from trade_tariff_reference.documents.tasks import generate_document as generate_document_task
+from trade_tariff_reference.documents.tasks import generate_fta_document as generate_document_task
 from trade_tariff_reference.documents.utils import update_agreement_document_status
 
 
