02cf13fc12d6c634e9a50696f8a2297c8d2dbb3a
Marcus Patino Pan <marcus.patino-pan@digital.trade.gov.uk>
2019-08-21 14:40:25 +0100

    Update setup script to load data

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ if [ -z "$1" ]; then
     python manage.py createsuperuser
 fi
 
-python manage.py loaddata agreement extendedquotas
+python manage.py loaddata agreement extendedquotas latin_terms special_notes seasonalquota seasonalquotaseasons
 python manage.py create_all_fta_documents --force --background
