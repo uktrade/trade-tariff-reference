2f40069140bda8ec7a3163723d0ed7e9331d89bd
Marcus Patino Pan <marcus.patino-pan@digital.trade.gov.uk>
2019-09-17 12:49:09 +0100

    Fix for create fta

--- trade_tariff_reference/schedule/tests/test_forms.py
+++ trade_tariff_reference/schedule/tests/test_forms.py
@@ -23,6 +23,7 @@ def test_empty_create_agreement_form():
         'agreement_date_year': ['This field is required.'],
         'agreement_name': ['This field is required.'],
         'country_codes': ['This field is required.'],
+        'country_name': ['This field is required.'],
         'slug': ['This field is required.'],
         'version': ['This field is required.'],
     }
@@ -79,6 +80,7 @@ def test_update_agreement_errors(update_post_data, expected_errors):
         'agreement_date_day': 1,
         'agreement_date_month': 2,
         'agreement_date_year': 2029,
+        'country_name': 'Country',
     }
     data.update(update_post_data)
     qs = urlencode(data, True)
@@ -98,6 +100,7 @@ def test_create_agreement_slug_already_exists():
         'agreement_date_day': 1,
         'agreement_date_month': 2,
         'agreement_date_year': 2029,
+        'country_name': 'Country',
     }
     qs = urlencode(data, True)
     post_data = QueryDict(qs)
