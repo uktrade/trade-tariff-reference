4a43de28f96f535573092f348beee23c0edc66c1
Marcus Patino Pan <marcus.patino-pan@digital.trade.gov.uk>
2019-10-14 11:31:06 +0100

    Update read me

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,4 +7,8 @@ if [ -z "$1" ]; then
 fi
 
 run "python manage.py loaddata agreement extendedquotas latin_terms special_notes chapters chapter_notes"
-run "python manage.py create_all_fta_documents --force --background"
+run "python manage.py create_all_fta_documents --force"
+run "python manage.py create_mfn_documents classification --force"
+run "python manage.py create_mfn_documents schedule --force"
+run "python manage.py create_mfn_master_document classification --force"
+run "python manage.py create_mfn_master_document schedule --force"
