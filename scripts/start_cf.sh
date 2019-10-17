819c25dd30092f5527717f57551f13d2a761a1f7
Marcus Patino Pan <marcus.patino-pan@digital.trade.gov.uk>
2019-10-17 16:38:36 +0100

    Wrap waitserve

--- scripts/start_cf.sh
+++ scripts/start_cf.sh
@@ -8,5 +8,5 @@ run "python manage.py migrate --database tariff --noinput"
 
 
 if [[ -z "${DEVELOPMENT_SERVER}" ]]; then
-    waitress-serve --port=$PORT config.wsgi:application
+    run "waitress-serve --port=$PORT config.wsgi:application"
 fi
