538443b2d301121869f0ea85a904af222531c534
Marcus Patino Pan <marcus.patino-pan@digital.trade.gov.uk>
2019-10-17 15:22:29 +0100

    Update generate_documents.sh

--- scripts/generate_documents.sh
+++ scripts/generate_documents.sh
@@ -2,6 +2,6 @@
 
 source ./scripts/functions.sh
 
-run "./scripts/setup.sh skipsuperuser"
 run "python manage.py create_all_fta_documents --force"
-run "./scritps/compile_assets.sh"
+run "python manage.py create_mfn_document classification --force"
+run "python manage.py create_mfn_document schedule --force"
