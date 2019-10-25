5a1a51e10da8b3eeef6dcdc46f5da68b96d4de65
Marcus Patino Pan <marcus.patino-pan@digital.trade.gov.uk>
2019-10-25 19:17:47 +0100

    Update settings

---
+++ config/settings/database.py
@@ -0,0 +1,36 @@
+import ssl
+
+from .base import *
+
+import dj_database_url
+
+
+def sort_database_config(database_list):
+    config = {}
+    for database in database_list:
+        config[database['name']] = database['credentials']['uri']
+    return config
+
+
+VCAP_SERVICES = env.json('VCAP_SERVICES')
+
+VCAP_DATABASES = sort_database_config(VCAP_SERVICES['postgres'])
+
+DEFAULT_DATABASE_URL = VCAP_DATABASES[env('POSTGRES_DB')]
+TARIFF_DATABASE_URL = VCAP_DATABASES[env('UK_TARIFF_DB')]
+
+REDIS_URL = VCAP_SERVICES['redis'][0]['credentials']['uri']
+
+
+DATABASES = {
+    'default': dj_database_url.parse(DEFAULT_DATABASE_URL),
+    'tariff': dj_database_url.parse(TARIFF_DATABASE_URL)
+}
+
+
+CELERY_BROKER_URL = f'{REDIS_URL}/{CELERY_REDIS_INDEX}'
+CELERY_RESULT_BACKEND = CELERY_BROKER_URL
+CELERY_BROKER_USE_SSL = {
+    'ssl_cert_reqs': ssl.CERT_REQUIRED
+}
+CELERY_REDIS_BACKEND_USE_SSL = CELERY_BROKER_USE_SSL
